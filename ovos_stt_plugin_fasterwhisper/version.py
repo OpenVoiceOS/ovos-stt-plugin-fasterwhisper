7cb3782c67c285cf0d127644d4ad8f7592147740
JarbasAl <JarbasAl@users.noreply.github.com>
2024-09-13 20:58:15 +0000

    Increment Version to 0.0.1a11

--- ovos_stt_plugin_fasterwhisper/version.py
+++ ovos_stt_plugin_fasterwhisper/version.py
@@ -1,7 +1,6 @@
-# The following lines are replaced during the release process.
 # START_VERSION_BLOCK
 VERSION_MAJOR = 0
 VERSION_MINOR = 0
 VERSION_BUILD = 1
-VERSION_ALPHA = 10
+VERSION_ALPHA = 11
 # END_VERSION_BLOCK
